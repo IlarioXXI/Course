ca8bab13726885ddf9cc1197364e2e77a2fb8783
Ilario <ilario01donhghia@hotmail.com>
2025-03-07 10:14:45 +0100

    implemented category repo

---
+++ Course.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>d
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
--- Course.DataAccess/Repository/IRepository/IRepository.cs
+++ Course.DataAccess/Repository/IRepository/IRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Course.DataAccess.Repository.IRepository
 {
-    internal interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         IEnumerable<T> GetAll();
 
